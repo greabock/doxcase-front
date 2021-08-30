5258ad07233cec472de00aee1eba5679d52d57a5
Gleb <volos555@gmail.ocm>
2021-08-31 01:40:35 +0300

    Пустой поиск.

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -38,7 +38,7 @@ export default {
     bottom: 0;
     left: 0;
     right: 0;
-    background-color: rgba(255, 255, 255, 0.9);
+    background-color: #fff;
 }
 .content-loader__cont {
     font-size: 26px;
