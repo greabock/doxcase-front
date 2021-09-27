6fb4c470e850d00f8d283aa16bcc345c8662bfb5
Gleb <volos555@gmail.ocm>
2021-09-27 13:33:48 +0300

    Список групп переделан VBox

--- src/pages/ProfilePage/GroupsTab.vue
+++ src/pages/ProfilePage/GroupsTab.vue
@@ -3,26 +3,34 @@
     <div
         class="btns-group-sm groups-users-buttons"
     >
-        <button
+        <VBox
             v-for="group in allGroups"
-            @click="currentGroup = group"
-            class="btn-filter"
-            :class="{active: group.id === currentGroup.id}"
             :key="group?.id"
-        >
-            {{ group?.name }}
-            <svg @click.stop="() => setGroupToRemove(group)" class="icon icon-close">
-                <use xlink:href="/img/svg/sprite.svg#close"></use>
-            </svg>
-        </button>
+            :title="group?.name"
+            :active="group.id === currentGroup.id"
+            @click="currentGroup = group"
+            @delete="setGroupToRemove(group)"
+        />
     </div>
+<!--        <button-->
+<!--            v-for="group in allGroups"-->
+<!--            @click="currentGroup = group"-->
+<!--            class="btn-filter"-->
+<!--            :class="{active: group.id === currentGroup.id}"-->
+<!--            :key="group?.id"-->
+<!--        >-->
+<!--            {{ group?.name }}-->
+<!--            <svg @click.stop="() => setGroupToRemove(group)" class="icon icon-close">-->
+<!--                <use xlink:href="/img/svg/sprite.svg#close"></use>-->
+<!--            </svg>-->
+<!--        </button>-->
+<!--    </div>-->
     <div class="mb-4 groups-users-add-button">
         <div @click="isAddModalVisible = true" class="btn-add">
             <div class="btn-add__plus"></div>
             <div class="btn-add__text">Добавить группу</div>
         </div>
     </div>
-
     <group-users-list
         :allUsers="allUsers"
         :propGroup="currentGroup"
@@ -126,6 +134,7 @@
 <script>
 import {ref, computed, onMounted} from 'vue';
 import VButton from '@/ui/VButton';
+import VBox from '@/ui/VBox';
 import ModalWindow from '@/components/ModalWindow';
 import GroupUsersList from '@/pages/ProfilePage/GroupUsersList';
 import * as yup from 'yup';
@@ -138,7 +147,8 @@ export default {
     components: {
         VButton,
         ModalWindow,
-        GroupUsersList
+        GroupUsersList,
+        VBox
     },
     setup() {
 
