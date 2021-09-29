6c591db64ab46838c05e7e356085609c17c84e53
Gleb <volos555@gmail.ocm>
2021-09-29 12:14:40 +0300

    Вурстка плашки

---
+++ src/components/KeyAlert.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class='key-alert-wrapper'>
+        <div class='row'></div>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+</style>
