49fa33f41ab110a50625979f98c6be2fc83c0f95
Gleb <volos555@gmail.ocm>
2021-12-21 18:07:58 +0300

    Заленый цвет. убран футер

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,12 +1,13 @@
 <template>
-    <footer class="footer">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col">© Doxcase</div>
-                <div class="col-auto"><router-link to="#"> OKTA DIGITAL </router-link></div>
-            </div>
-        </div>
-    </footer>
+    <div></div>
+<!--    <footer class="footer">-->
+<!--        <div class="container-fluid">-->
+<!--            <div class="row">-->
+<!--                <div class="col">© Doxcase</div>-->
+<!--                <div class="col-auto"><router-link to="#"> OKTA DIGITAL </router-link></div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </footer>-->
 </template>
 <script>
 export default {
